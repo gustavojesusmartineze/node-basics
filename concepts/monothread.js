a42eac514d2589cf6c2e27c776796a2870a63ece
Gustavo Martínez <gustavomartinez1990@gmail.com>
2022-04-17 15:20:00 -0400

    feat(concepts): add monothread examples

---
+++ concepts/monothread.js
@@ -0,0 +1,15 @@
+console.log('Hello Clarice node');
+
+let i = 0;
+
+setInterval(function () {
+  console.log(i);
+  if (i===5) {
+    var a = 3 + z;
+  }
+  
+  i++;
+}, 1000);
+
+
+console.log('Second instruction');
