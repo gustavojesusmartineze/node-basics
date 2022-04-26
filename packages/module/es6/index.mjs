b4128105e530c7b652ca55568e8a6a5bb1f5d811
Gustavo Martínez <gustavomartinez1990@gmail.com>
2022-04-26 17:14:53 -0400

    feat(packages): add module exports examples

---
+++ packages/module/es6/index.mjs
@@ -0,0 +1,6 @@
+import moduleObj from './module.mjs'
+
+// moduleObj.greet();
+
+console.log(moduleObj.prop1);
+moduleObj.greet()
---
+++ packages/module/es6/module.mjs
@@ -0,0 +1,9 @@
+function greet() {
+  console.log('Hello module test');
+}
+
+// export default greet;
+export default {
+  greet,
+  prop1: 'Hello Clarice'
+}
